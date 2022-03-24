6ae87ab5a94edb5bf702755862364d487f13ca1c
Akash-Vijay <akashvijay789@gmail.com>
2022-03-24 14:07:52 +0530

    Added Decrement button

--- function.js
+++ function.js
@@ -2,6 +2,13 @@ let count = 0;
 let value = document.getElementById("count");
 
 function increment() {
-  count = count + 1;
+  count += 1;
   value.innerText = count;
+  return count;
+}
+
+function decrement() {
+  count -= 1;
+  value.innerText = count
+  return count;
 }
