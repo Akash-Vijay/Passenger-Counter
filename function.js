92b56c2b1f9a5b01ef4e990c96f7f98ec6ddcf4d
Akash-Vijay <akashvijay789@gmail.com>
2022-03-24 21:55:17 +0530

    Added Counter Reset

--- function.js
+++ function.js
@@ -5,18 +5,20 @@ let prevData = document.getElementById("prev-data");
 //Increment function
 function increment() {
   count += 1;
-  value.innerText = count;
+  value.textContent = count;
   return count;
 }
 
 //Decrement function
 function decrement() {
   count -= 1;
-  value.innerText = count;
+  value.textContent = count;
   return count;
 }
 
 //Save fucntion
 function save() {
   prevData.textContent += count + " | ";
+  count = 0;
+  value.textContent = 0;
 }
