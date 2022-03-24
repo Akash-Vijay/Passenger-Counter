e69b7e54649c5cd867d194884284eb868c646d78
Akash-Vijay <akashvijay789@gmail.com>
2022-03-24 21:17:14 +0530

    Added Save Button

--- function.js
+++ function.js
@@ -1,12 +1,17 @@
 let count = 0;
 let value = document.getElementById("count");
 
+
+
+//Increment function
 function increment() {
   count += 1;
   value.innerText = count;
   return count;
 }
 
+
+//Decrement function
 function decrement() {
   count -= 1;
   value.innerText = count
